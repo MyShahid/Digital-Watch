64546cdfac02a029ab2596add4204a0104a8f68c
MyShahid <shahid9167411@gmail.com>
2024-08-12 23:20:46 +0530

    initial commit

---
+++ indes.js
@@ -0,0 +1,11 @@
+
+const currentTime=()=>{
+    let currTime = new Date().toLocaleTimeString();
+    document.getElementById("clock").innerText = currTime;
+};
+
+currentTime();
+
+setInterval(() => {
+    currentTime();
+}, 1000);
